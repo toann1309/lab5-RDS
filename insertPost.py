b346bbeacff1c4a2147e23691c772ec5a5874cea
toann1309 <toanchau30@gmail.com>
2022-10-30 17:17:54 +0700

    first

---
+++ insertPost.py
@@ -0,0 +1,11 @@
+import insertUtil as ut
+import psycopg2
+
+
+#Tạo kết nối với RDS PostgreSQL và thêm dữ liệu vào
+conn = psycopg2.connect(database='covid19_postgresql', user='postgres', password='12345678', host='database-postgresql.cjlexxkdghse.us-east-1.rds.amazonaws.com', port='5432')
+print('Open DB successfully')
+ut.insert(conn)
+ut.select(conn)
+conn.close()
+print('Successfully')
